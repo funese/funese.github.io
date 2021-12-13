6749010e40cb3b38bef88a05f149e4fa2a449741
emiliano <funes.emiliano@outlook.com>
2021-12-12 22:10:55 -0300

    Add navbar

--- main.js
+++ main.js
@@ -57,12 +57,6 @@
     };
 })(jQuery);
 
-
-
-
-
-
-
 // this makes the height of each page equal to the height of the window
 // $('.page').css('height', $( window ).height());
 
@@ -111,39 +105,3 @@
         }
     })
 })(jQuery);
-
-hash = function (h) {
-    if (history.pushState) {
-        history.pushState(null, null, h);
-    } else {
-        location.hash = h;
-    }
-}
-
-
-$(function () {
-
-    $(".progress").each(function () {
-
-        var value = $(this).attr('data-value');
-        var left = $(this).find('.progress-left .progress-bar');
-        var right = $(this).find('.progress-right .progress-bar');
-
-        if (value > 0) {
-            if (value <= 50) {
-                right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-            } else {
-                right.css('transform', 'rotate(180deg)')
-                left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-            }
-        }
-
-    })
-
-    function percentageToDegrees(percentage) {
-
-        return percentage / 100 * 360
-
-    }
-
-});
